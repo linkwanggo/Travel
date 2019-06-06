33e8cc6e31266a32211974593b63eab76f2c0390
linkwanggo <linkwanggo@gmail.com>
2019-06-06 19:33:16 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import './assets/styles/reset.css' // 定义使用的样式
+import './assets/styles/border.css' // 解决部分移动端页面border 1像素显示问题
+import fastClick from 'fastclick'
+
+// 解决部分浏览器点击响应延迟问题
+fastClick.attach(document.body)
 
 Vue.config.productionTip = false
 
@@ -10,6 +16,8 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>'
 })
